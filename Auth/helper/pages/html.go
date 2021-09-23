68d34b1201fb93327d346fa57cf9a50aec3d770e
ankitksh81 <ankitksh81@gmail.com>
2021-09-23 16:52:23 +0530

    login functions for oauth

---
+++ Auth/helper/pages/html.go
@@ -0,0 +1,20 @@
+package pages
+
+// IndexPage renders the html content for the index page.
+
+const IndexPage = `
+<html>
+	<head>
+		<title>Oauth-2 Test</title>
+	</head>
+	<body>
+		<h2>Oauth-2 Test</h2>
+		<p>
+			Login with the following,
+		</p>
+		<ul>
+			<li><a href="/login-gl">Google</a></li>
+		</ul>
+	</body>
+</html>
+`
