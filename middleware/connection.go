999f9cec77f00115b3e5fe3016bd70b28ea6ad5d
ankitksh81 <ankitksh81@gmail.com>
2021-09-30 21:12:23 +0530

    fixed db connecting issue

--- middleware/connection.go
+++ middleware/connection.go
@@ -2,38 +2,32 @@ package middleware
 
 import (
 	"database/sql"
-	"fmt"
 
+	"github.com/ankitksh81/nyke/logger"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
-)
-
-var (
-	host     = viper.GetString("dbConfig.host")
-	port     = viper.GetString("dbConfig.port")
-	user     = viper.GetString("dbConfig.user")
-	password = viper.GetString("dbConfig.password")
-	dbname   = viper.GetString("dbConfig.dbname")
 )
 
 var DB *sql.DB
 
 func CreateConnection() *sql.DB {
-
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", psqlInfo)
+	logger.Log.Info("Connecting to database...")
+	var ConStr = viper.GetString("dbConfig.constr")
+	db, err := sql.Open("postgres", ConStr)
 	if err != nil {
+		logger.Log.Info("Could not create connection to the database" + err.Error())
 		panic(err)
 	}
+	defer db.Close()
 
 	err = db.Ping()
 	if err != nil {
+		logger.Log.Info("Error connecting to the database" + err.Error())
 		panic(err)
 	}
 
 	DB = db
 
-	fmt.Println("Successfully connected!")
+	logger.Log.Info("Successfully connected to the database!")
 	return db
 }
