d3d0d486499f50b16b13f7daeb652fc6e52b8865
ankitksh81 <ankitksh81@gmail.com>
2021-12-03 20:45:37 +0530

    refactor

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -4,15 +4,13 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-/* Hash password */
-
+// Function to hash password
 func HashPassword(password string) (string, error) {
 	hashedPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(hashedPass), err
 }
 
-/* Compare hashed password password */
-
+// Function to compare password hash
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
