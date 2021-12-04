461c153c081160f8786152b2679c4ab9fe0a01f0
ankitksh81 <ankitksh81@gmail.com>
2021-12-04 23:18:31 +0530

    added validation for jwt

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,13 +1,19 @@
 package middleware
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"strings"
 	"time"
 
+	"github.com/ankitksh81/nyke/logger"
+	"github.com/ankitksh81/nyke/models"
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/spf13/viper"
 )
 
+// function to generate jwt token
 func GenerateJWT(user_id string) (string, error) {
 	secretKey := viper.GetString("jwtSecret")
 	var mySigningKey = []byte(secretKey)
@@ -26,4 +32,50 @@ func GenerateJWT(user_id string) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+// function to verify token
+func IsAuthorized(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		authHeader := r.Header.Get("Authorization")
+		bearerToken := strings.Split(authHeader, " ")
+
+		var mySigningKey = viper.GetString("jwtSecret")
+
+		if len(bearerToken) == 2 {
+			authToken := bearerToken[1]
+
+			token, err := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error in parsing")
+				}
+				return []byte(mySigningKey), nil
+			})
+			if err != nil {
+				errMsg := &models.Error{
+					Message: "Your token has been expired!",
+				}
+				JSONError(w, errMsg, 401)
+				logger.Log.Error("Token Error: " + err.Error())
+				return
+			}
+
+			if token.Valid {
+				handler.ServeHTTP(w, r)
+				return
+			} else {
+				err := &models.Error{
+					Message: "You are not authorized!",
+				}
+				JSONError(w, err, 401)
+			}
+		} else {
+			err := &models.Error{
+				Message: "Invalid token!",
+			}
+			JSONError(w, err, 401)
+		}
+
+	}
 }
