7cbf32b243630342c2f8625c28514bc86ec0f8ea
ankitksh81 <ankitksh81@gmail.com>
2021-12-08 01:33:10 +0530

    refactor & added user check

--- auth/google.go
+++ auth/google.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ankitksh81/nyke/logger"
 	"github.com/ankitksh81/nyke/middleware"
 	"github.com/ankitksh81/nyke/models"
+	"github.com/ankitksh81/nyke/services/api"
 
 	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
@@ -20,14 +21,14 @@ var (
 		ClientID:     "",
 		ClientSecret: "",
 		RedirectURL:  "http://localhost:8080/callback",
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"},
-		Endpoint:     google.Endpoint,
+		Scopes: []string{"https://www.googleapis.com/auth/userinfo.profile",
+			"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint: google.Endpoint,
 	}
-	oauthStateStringGl = ""
+	oauthStateStringGl string
 )
 
 // InitializeOAuthGoogle Function
-
 func InitializeOAuthGoogle() {
 	oauthConfigGl.ClientID = viper.GetString("google.clientID")
 	oauthConfigGl.ClientSecret = viper.GetString("google.clientSecret")
@@ -35,7 +36,6 @@ func InitializeOAuthGoogle() {
 }
 
 // HandleGoogleLogin Function
-
 func HandleGoogleLogin(w http.ResponseWriter, r *http.Request) {
 	HandleLogin(w, r, oauthConfigGl, oauthStateStringGl)
 }
@@ -46,11 +46,12 @@ type Response struct {
 }
 
 // CallBackFromGoogle Function
-func CallBackFromGoogle(w http.ResponseWriter, r *http.Request) {
-	logger.Log.Info("Callback-gl..")
+const oauth_url = "https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token="
 
+func CallBackFromGoogle(w http.ResponseWriter, r *http.Request) {
+	// logger.Log.Info("Callback-gl..")
 	state := r.FormValue("state")
-	logger.Log.Info(state)
+	// logger.Log.Info(state)
 	if state != oauthStateStringGl {
 		logger.Log.Info("Invalid oauth state, expected " + oauthStateStringGl + ", got " + state + "\n")
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
@@ -58,26 +59,33 @@ func CallBackFromGoogle(w http.ResponseWriter, r *http.Request) {
 	}
 
 	code := r.FormValue("code")
-	logger.Log.Info(code)
+	// logger.Log.Info(code)
 
 	if code == "" {
 		logger.Log.Warn("Code not found..")
-		w.Write([]byte("Code Not Found to provide AccessToken..\n"))
+		// w.Write([]byte("Code Not Found to provide AccessToken..\n"))
 		reason := r.FormValue("error_reason")
 		if reason == "user_denied" {
-			w.Write([]byte("User has denied Permission.."))
+			// w.Write([]byte("User has denied Permission.."))
+			errMsg := "User has denied Permission!"
+			middleware.JSONError(w, errMsg, 401)
+		} else {
+			errMsg := "Could not get the access token."
+			middleware.JSONError(w, errMsg, 400)
 		}
 	} else {
 		token, err := oauthConfigGl.Exchange(oauth2.NoContext, code)
 		if err != nil {
 			logger.Log.Error("oauthConfigGl.Exchange() failed with " + err.Error() + "\n")
+			errMsg := "Could not get the access token."
+			middleware.JSONError(w, errMsg, 400)
 			return
 		}
-		logger.Log.Info("TOKEN>> AccessToken>> " + token.AccessToken)
-		logger.Log.Info("TOKEN>> Expiration Time>> " + token.Expiry.String())
-		logger.Log.Info("TOKEN>> RefreshToken>> " + token.RefreshToken)
+		// logger.Log.Info("TOKEN>> AccessToken>> " + token.AccessToken)
+		// logger.Log.Info("TOKEN>> Expiration Time>> " + token.Expiry.String())
+		// logger.Log.Info("TOKEN>> RefreshToken>> " + token.RefreshToken)
 
-		resp, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=" + url.QueryEscape(token.AccessToken))
+		resp, err := http.Get(oauth_url + url.QueryEscape(token.AccessToken))
 		if err != nil {
 			logger.Log.Error("Get: " + err.Error() + "\n")
 			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
@@ -92,12 +100,40 @@ func CallBackFromGoogle(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		logger.Log.Info("parseResponseBody: " + string(response) + "\n")
+		// logger.Log.Info("parseResponseBody: " + string(response) + "\n")
 
+		// unmarshal response
 		var authRes models.AuthResponse
 		json.Unmarshal(response, &authRes)
-		middleware.RegisterFromAuth(w, &authRes)
-		// w.Write([]byte(string(response)))
+
+		// check if user already exists
+		userFound, err := middleware.CheckUserExist(authRes.Email)
+		if err != nil {
+			middleware.Error500(w)
+		}
+
+		// if user not found, register the user
+		if !userFound {
+			api.RegisterAuth(w, &authRes)
+		} else {
+			sqlQuery := `SELECT user_id FROM users WHERE email = $1`
+			var user_id string
+			err := middleware.DB.QueryRow(sqlQuery, authRes.Email).Scan(&user_id)
+			if err != nil {
+				middleware.Error500(w)
+				return
+			}
+			jwtToken, err := middleware.GenerateJWT(user_id)
+			if err != nil {
+				middleware.Error500(w)
+				return
+			}
+
+			token := &middleware.JwtToken{
+				Token: jwtToken,
+			}
+			json.NewEncoder(w).Encode(token)
+		}
 		return
 	}
 }
